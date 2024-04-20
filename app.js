7e9ad4bae77b05288114175c4b04cc0858efbeb2
alishbanazar <alishbanazarmuhammadabbasi@gmail.com>
2024-04-20 21:49:00 +0500

    alert

---
+++ app.js
@@ -0,0 +1,15 @@
+//task-1//
+alert("Assalam-o-Alikum Everyone ")
+
+//task-2//
+alert("Error! Please enter a valid password");
+//task-3//
+alert(`Welcome to JS Land........
+Happy Coding!`);
+// task-4//
+alert("Welcome to JS Land.......");
+//task-4 part-b//
+alert(`Happy Coding! 
+Prevent this page from creating additional dialogs`);
+//task-5//
+alert("Hello.... I can run JS through my web browser's console")
