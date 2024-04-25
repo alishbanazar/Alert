202f8d0853855f452ba09b50fbdd05cec3e363da
alishbanazar <alishbanazarmuhammadabbasi@gmail.com>
2024-04-25 23:14:35 +0500

    done variable

---
+++ Chp#2 Assignment/app.js
@@ -0,0 +1,39 @@
+// question#1
+var userName;
+
+// question#2
+var myName = "Alisba Nazar Muhammad Abbasi";
+
+// question#3 (a)
+var message;
+
+// question#3 (b)
+var message = "Hello World";
+
+// question#3 (c)
+alert(message);
+
+// question#4
+alert("Alishba Nazar Muhammad Abbasi");
+alert("Saylani Mass IT Training Student");
+alert("Certified Mobile App Development");
+
+// question#5
+var fastFood = `PIZZA
+PIZZ
+PIZ
+PI
+P`
+alert(fastFood);
+
+// question#6
+var email = "alishbanazarmuhammad@gmail.com";
+alert("My e-mail address is: " + email);
+
+// question#7
+var book = "A smarter way to learn Javascript"
+alert("I am trying to learn from the book "+ book);
+
+// question#8
+document.write("Yah! I can write HTML content through Javascript");
+
--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 //task-1//
-alert("Assalam-o-Alikum Everyone ")
+// alert("Assalam-o-Alikum Everyone ")
 
 //task-2//
-alert("Error! Please enter a valid password");
+// alert("Error! Please enter a valid password");
 //task-3//
-alert(`Welcome to JS Land........
-Happy Coding!`);
+// alert(`Welcome to JS Land........
+//  Happy Coding!`);
 // task-4//
-alert("Welcome to JS Land.......");
+// alert("Welcome to JS Land.......");
 //task-4 part-b//
-alert(`Happy Coding! 
-Prevent this page from creating additional dialogs`);
+// alert(`Happy Coding! 
+// Prevent this page from creating additional dialogs`);
 //task-5//
-alert("Hello.... I can run JS through my web browser's console")
+// alert("Hello.... I can run JS through my web browser's console")
 
-console.log("Hello World")
+// console.log("Hello World")
