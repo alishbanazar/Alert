3ac8db64e263cfd2bdb9ad9213d09aace238eb08
alishbanazar <alishbanazarmuhammadabbasi@gmail.com>
2024-05-07 22:42:23 +0500

    math expression done

---
+++ Chp#5 Assignments/app.js
@@ -0,0 +1,58 @@
+// question no#1
+var num1 = "3";
+var num2 = "5";
+// document.write( "The sum of " + num1 + " and " + num2 +" is 5");
+
+// question no#2
+// document.write(num1-num2)
+
+// document.write(num1*num2)
+
+// document.write(num1%num2)
+
+// question no#3
+// a. Declare a variable
+var myVariable;
+
+// b. Show the value of the variable in the browser
+document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+// c. Initialize the variable with some number
+myVariable = 10;
+
+// Show the updated value of the variable in the browser
+document.write("Value after variable initialization is: " + myVariable + "<br>");
+
+// Increment the variable
+++myVariable;
+
+//  Show the value of variable in your browser
+ document.write("Value after increment is: " + myVariable + "<br>")
+
+//  Add 7 to the variable
+myVariable = 10+7;
+var result = "17"
+
+// Show the value of variable in your browser
+document.write("Value after addition is: " + result + "<br>" )
+
+// Decrement the variable
+myVariable--;
+
+// value of variable in your browser
+document.write("Value after decrement is: " + myVariable + "<br>")
+
+// dividing the variable’s value by 3
+myVariable = 16%3
+
+// output
+document.write("Output:" + "The remainder is " + myVariable + "<br>")
+
+// question no#4
+var ticketPrice = "600"
+var totalPrice = ticketPrice*5 
+document.write("Total cost to buy 5 tickets to a movie is " + totalPrice + "PKR" + "<br>")
+
+// question no#5
+
+
