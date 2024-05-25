e30a4d8761e80ee14224ebf349739e1fae6fdeb4
alishbanazar <alishbanazarmuhammadabbasi@gmail.com>
2024-05-25 16:37:23 +0500

    arrays

---
+++ Chp#14 to 1 6/app.js
@@ -0,0 +1,23 @@
+    //   -------------ARRAYS------------
+
+// 1. Declare an empty array using JS literal notation to store student names in future.
+// var studentName = [];
+
+// 2. Declare an empty array using JS object notation to store student names in future.
+
+// var students = new Array ();
+
+// 3. Declare and initialize a strings array.
+
+// var books = [" mathematice ", " English " , " Urdu " , " General-Sciencs "];
+// document.write(" <h4>String Arrays: </h4> " + books + "<br>" )
+
+// 4. Declare and initialize a numbers array.
+// var num =[2,4,6,8,10];
+// document.write(" <h4> Number Arrays:</h4> " + num + "<br>")
+
+// 5. Declare and initialize a boolean array.
+// var boolean =[" true "," false ", " false " , " true "];
+// document.write("<h4> Boolean Arrays: </h4> " + boolean + "<br>");
+
+// 6. Declare and initialize a mixed array.
